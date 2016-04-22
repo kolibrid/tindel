7729bdf51e4316243202a2b07a44a2e12c1e5f5b
Alvaro <kolibrid111@gmail.com>
2016-04-22 19:53:04 +0200

    UPDATED: First commit

---
+++ Tindel/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  Tindel
+//
+//  Created by Álvaro Martínez Fernández on 22/4/16.
+//  Copyright © 2016 Geekshub. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let defaults = NSUserDefaults.standardUserDefaults();
+        defaults.setValue("email@email.com", forKey: "email")
+        defaults.synchronize()
+        var email = defaults.objectForKey("email")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
